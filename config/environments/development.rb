68b46b7653743fd521ee23cd4ee18fa2dbb8d00a
Greidy <greidy.valdes@gmail.com>
2018-08-21 18:31:00 -0400

    blablabla

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,10 +27,11 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -45,6 +46,16 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'bluepbooking',
+    password:             'BPB606^)^',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
   Paperclip.options[:command_path] = "C:/Windows/System32"
   Paperclip.options[:command_path] = "C:/Program Files (x86)/GnuWin32/bin"
   # Raises error for missing translations
