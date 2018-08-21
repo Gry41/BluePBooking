963c31f961ab3f5f379f2e3d36e8d13af8f58cf0
Greidy <greidy.valdes@gmail.com>
2018-08-21 17:54:40 -0400

    blablabla

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,11 +94,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    host: smtp.gmail.com
-    port: 587 
-    protocol: tls
-    user: bluepbooking@gmail.com
-    password:  BPB606^)^
+    host: 'smtp.gmail.com',
+    port: 587 ,
+    protocol: 'tls',
+    user: 'bluepbooking@gmail.com',
+    password:  'BPB606^)^'
 
   }
   config.action_mailer.default_url_options = { :host => 'https://mighty-headland-81697.herokuapp.com' }
