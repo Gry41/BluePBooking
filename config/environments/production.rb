8f9f5a491379bb405bce0bf710db6c3223975dc7
Greidy <greidy.valdes@gmail.com>
2018-08-21 18:32:11 -0400

    blablabla

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://mighty-headland-81697.herokuapp.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'mighty-headland-81697.herokuapp.com', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
