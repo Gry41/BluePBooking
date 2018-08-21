8ddf777e70940eeaa2d10fe6e79770686f760a3a
Greidy <greidy.valdes@gmail.com>
2018-08-21 17:12:48 -0400

    blablabla

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.action_mailer.default_url_options = { :host => 'mighty-headland-81697.herokuapp.com' }
   # config.serve_static_assets = true
   # config.assets.compile = true
   # config.assets.digest = true
