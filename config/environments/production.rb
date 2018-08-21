00862ab933a471c18d869f871553875a8ce514db
Greidy <greidy.valdes@gmail.com>
2018-08-21 18:18:39 -0400

    blablabla

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.default_url_options = { host: 'https://mighty-headland-81697.herokuapp.com', port: 3000 }
-  
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'bluepbooking',
+    password:             'BPB606^)^',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 end
