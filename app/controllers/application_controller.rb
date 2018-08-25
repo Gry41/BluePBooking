02011cd0d1e0c6edfce6d08ae1c270d3a34bd31e
Greidy <greidy.valdes@gmail.com>
2018-08-25 14:22:34 -0400

    Fd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, :except => [:show, :index,:home, :query,:gallery, :mail,:all_points,:mail_us]
   contact = Contact.all.each
-  contact.next
-  @face_link = contact.next
-  @insta_link = contact.next
-  
+  b=contact.next
+  if b 
+    @face_link = contact.next
+    if @face_link 
+      @insta_link = contact.next
+    end
+  end
 end
