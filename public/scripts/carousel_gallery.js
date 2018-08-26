d5b2d1cf23cc18b93137e73cd2ca56ad76928360
Greidy <greidy.valdes@gmail.com>
2018-08-25 23:43:57 -0400

    gf

--- public/scripts/carousel_gallery.js
+++ public/scripts/carousel_gallery.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
        $('.carousel').carousel();
 
-    $('.carousel.carousel-slider').carousel({
-      fullWidth: true
-   });
+  //   $('.carousel.carousel-slider').carousel({
+  //     fullWidth: true
+  //  });
 });
 
 
