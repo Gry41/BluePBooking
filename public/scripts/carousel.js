bdb2e8987b4d256e532af0da5025f5c188ff58e9
Greidy <greidy.valdes@gmail.com>
2018-08-25 20:15:05 -0400

    gf

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -9,15 +9,15 @@
 
    // move next carousel
    $('.moveNextCarousel').click(function(e){
-      e.preventDefault();
-      e.stopPropagation();
+      // e.preventDefault();
+      // e.stopPropagation();
       $('.carousel').carousel('next');
    });
 
    // move prev carousel
    $('.movePrevCarousel').click(function(e){
-      e.preventDefault();
-      e.stopPropagation();
+      // e.preventDefault();
+      // e.stopPropagation();
       $('.carousel').carousel('prev');
    });
     $('.carousel').unbind( "click" );
