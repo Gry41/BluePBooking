9498183bd394526bfddf36a69ae8302516ace261
Greidy <greidy.valdes@gmail.com>
2018-08-29 19:39:21 -0400

    cloud

--- app/models/taxi_photo.rb
+++ app/models/taxi_photo.rb
@@ -1,7 +1,7 @@
 class TaxiPhoto < ApplicationRecord
     has_attached_file :image,
-  path: ":rails_root/public/images/taxi_photo/:id/:filename",
-  url: "/images/taxi_photo/:id/:filename"
-
+    :storage => :cloudinary,
+    :path => 'taxi/:id/:filename'
+   
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- app/models/tour_photo.rb
+++ app/models/tour_photo.rb
@@ -1,7 +1,7 @@
 class TourPhoto < ApplicationRecord
     has_attached_file :image,
-    path: ":rails_root/public/images/tour_photo/:id/:filename",
-    url: "/images/tour_photo/:id/:filename"
-  
+    :storage => :cloudinary,
+    :path => 'tour/:id/:filename'
+   
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
